a3a6c138fbd57802f7ab70d315d288408df1b74f
Swetha <swetha.ojili@hitachiconsulting.com>
2017-07-06 11:54:19 +0530

    Apache 4j logger related changes.

--- src/com/hsat/utilities/LoggerUtility.java
+++ src/com/hsat/utilities/LoggerUtility.java
@@ -3,12 +3,23 @@ package com.hsat.utilities;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
 
 public class LoggerUtility {
 
 	public static String filename = "log.txt";
 	public static String filePath = System.getProperty("user.dir");
 	public static File file = new File(filePath + "\\" + filename);
+	ExtentReports logger;
+	Logger log4j;
 	
 	public LoggerUtility(){	
 						
@@ -30,5 +41,35 @@ public class LoggerUtility {
 	public File returnFile(){
 		return file;
 	}
+	
 
+	public void ExtentReport_init_Report(String className){
+		logger = ExtentReports.get(className);
+		String fname = new SimpleDateFormat("yyyyMMddHHm").format(new Date())+className+"_Report";
+		logger.init("C:\\Report\\"+fname+".html",true);
+		logger.startTest(className);		
+	}
+	
+	public void ExtentReport_info(String message){
+		logger.log(LogStatus.INFO, message);		
+	}
+	
+	public void ExtentReport_pass(String message){
+		logger.log(LogStatus.PASS, message);		
+	}
+	
+	public void ExtentReport_endTest(){
+		logger.endTest();
+	}
+	
+	public void log4j_intialize(String className){		
+		BasicConfigurator.configure();
+		PropertyConfigurator.configure("log4j.properties");
+		log4j = Logger.getLogger(className);		
+	}
+	
+	public void log4j_loginfo(String message){		
+		log4j.info(message);
+	}
+	
 }
